5a7ba072a8d06ca35ad4d8ecf50a74bb51dabf0a
Bertomalone <aprilianoroberto@gmail.com>
2019-03-06 01:18:28 +0800

    change color using input range

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let upValue = 0, x;
 
 // create element with JS
 const addNewButton = document.createElement("button");
-const addNewTeks = document.createTextNode("New Change Color");
+const addNewTeks = document.createTextNode("Random Background Color");
 addNewButton.appendChild(addNewTeks);
 addNewButton.setAttribute('type', 'button');
 document.getElementsByTagName("h1")[2].after(addNewButton);
@@ -31,4 +31,25 @@ addNewButton.addEventListener("click", function(){
     const g = Math.round(Math.random() * 255 + 1);
     const b = Math.round(Math.random() * 255 + 1);
     document.getElementsByClassName("card")[2].style.backgroundColor = "rgb("+r+", "+g+", "+b+")";
+});
+
+// range rgb
+const rangeRed = document.querySelector("input[name=redRange]");
+const rangeGreen = document.querySelector("input[name=greenRange]");
+const rangeBlue = document.querySelector("input[name=blueRange]");
+
+function cologRGB(red, green, blue) {
+    return "rgb("+red+", "+green+", "+blue+")";
+} 
+
+rangeRed.addEventListener("input", function() {
+    document.getElementsByClassName("card")[3].style.backgroundColor = cologRGB(rangeRed.value, rangeGreen.value, rangeBlue.value);
+});
+
+rangeGreen.addEventListener("input", function() {
+    document.getElementsByClassName("card")[3].style.backgroundColor = cologRGB(rangeRed.value, rangeGreen.value, rangeBlue.value);
+});
+
+rangeBlue.addEventListener("input", function() {
+    document.getElementsByClassName("card")[3].style.backgroundColor = cologRGB(rangeRed.value, rangeGreen.value, rangeBlue.value);
 });
